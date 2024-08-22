ed0bb0d290e4fc612ec63daf0ad075bac73660d1
GOURAB PANDA <150692811+GOURAB-PANDA@users.noreply.github.com>
2024-08-22 19:15:25 +0800

    Time: 2 ms (99.12%) | Memory: 42.8 MB (83.51%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,23 +1,49 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int st = 0;
-        int end = s.length()-1;
-        while(st <= end){
-          char currFirst = s.charAt(st);
-          char currlast = s.charAt(end);
-          if(!Character.isLetterOrDigit(currFirst)){
-            st++;
-          }else if(!Character.isLetterOrDigit(currlast)){
-            end--;
-          }
-          else{
-            if(Character.toLowerCase(currFirst) != Character.toLowerCase(currlast)){
+        if (s == null || s.isEmpty()) {//ensuring array is not empty or contains null.
+            return true; 
+        }
+
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+           
+            while (start < end && !Character.isLetterOrDigit(s.charAt(start))) {//moving start until alphanumeric char found.
+                start++;
+            }
+            
+            while (start < end && !Character.isLetterOrDigit(s.charAt(end))) {//moving end for same as above.
+                end--;
+            }
+            //using two-pointer method to find if character at start is equals to end. If not returning false.
+            if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
                 return false;
             }
-            st++;
+            start++;
             end--;
-          }
         }
         return true;
+
+        /*less efficient method
+        //  if (s == null || s.isEmpty()) {
+        //     return true; // An empty string is considered a palindrome.
+        // }
+
+        // String t = s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");//the "^" works as negation and the statement means: if the element is not from the range in [], replace it with empty string.
+
+        // int start =0;
+        // int end = t.length()-1;
+
+        // while(start < end){//after removing alphanumeric cases if the array is empty, this will not be triggered.
+        //     if(t.charAt(start) == t.charAt(end)){
+        //         start++;
+        //         end--;
+        //     }else{
+        //         return false;
+        //     }
+        // }
+        // return true;
+        */
     }
 }
